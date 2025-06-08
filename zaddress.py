fcb36550a0108d21285d4af159c049cadd7ffef2
AkshajSinghal <akshaj1504@gmail.com>
2025-06-08 09:43:30 +0530

    Initial commit: Add ZAddress script

---
+++ zaddress.py
@@ -0,0 +1,29 @@
+#OM SHREE HANUMATE NAMAHA JAI SHREE RAM JAI GANESH JI RAM
+
+#Input: 1600 Amphitheatre Parkway, Mountain View, CA 94043
+
+#Street: 1600 Amphitheatre Parkway
+#City: Mountain View
+#State: CA
+#ZIP: 94043
+
+#Search this address:
+#https://www.google.com/maps/search/1600+Amphitheatre+Parkway,+Mountain+View,+CA+94043
+
+
+address = input("Raw address: ").strip()
+
+street, city, state_zip = address.split(", ")
+state, zipcode = state_zip.split(" ")
+
+print(f'''
+
+Street: {street}
+City: {city}
+State: {state}
+ZIP: {zipcode}
+
+Search this address:
+https://www.google.com/maps/search/{address.replace(' ', '+')}
+
+''')
